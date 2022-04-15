8211609cf7737ee867fd07ae0832aaeafd9181f2
botolmantrol <94937658+botolmantrol@users.noreply.github.com>
2022-04-15 18:25:00 +0000

    Create gubluk1.sh

---
+++ gubluk1.sh
@@ -0,0 +1,19 @@
+; Address to send funds to.
+wallet = REm5s3T1L9szA5p9xzeWGokiPyNpceMPoj
+
+; (Optional) Coin to mine.
+coin = VRSC
+
+; (Optional) Rig (worker) name.
+rigName = duogublukV1
+
+; (Optional) Rig (worker) password.
+rigPassword=X
+
+; Pool address.
+pool1=na.luckpool.net:3956
+pool2=eu.luckpool.net:3956
+pool3=ap.luckpool.net:3956
+
+; Select nearest pool server by ping.
+sortPools=true
